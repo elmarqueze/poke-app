4326382c3639973cada6745d577e37879bdc4757
Waldir Marquez POD <waldir.marquez@hirewithpod.com>
2023-03-23 18:39:40 -0500

    localstorage en mi pokeapp

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+    
+    const back = () => {
+        router.push('/pokemons')
+    }
+</script>
+
+<template>
+    <h1>404</h1>
+    <button @click="back" class="btn btn-outline-primary">Inicio</button>
+</template>
